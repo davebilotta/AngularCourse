6bc0480e0cfe2d151b5bc04e859ce8b441444007
David Bilotta <David.Bilota@sunlife.com>
2023-01-07 16:31:59 -0500

    Updated

--- math/src/app/equation/equation.component.ts
+++ math/src/app/equation/equation.component.ts
@@ -1,26 +1,26 @@
-import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
-import {MathValidators} from "../math-validators";
-import {delay, filter, scan} from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MathValidators } from '../math-validators';
+import { delay, filter, scan } from 'rxjs';
 
 @Component({
     selector: 'app-equation',
     templateUrl: './equation.component.html',
-    styleUrls: ['./equation.component.css']
+    styleUrls: ['./equation.component.css'],
 })
 export class EquationComponent implements OnInit {
-
     secondsPerSolution = 0;
 
-    mathForm = new FormGroup({
+    mathForm = new FormGroup(
+        {
             a: new FormControl(this.randomNumber()),
             b: new FormControl(this.randomNumber()),
-            answer: new FormControl('')
+            answer: new FormControl(''),
         },
-        [MathValidators.addition('answer', 'a', 'b')]);
+        [MathValidators.addition('answer', 'a', 'b')]
+    );
 
-    constructor() {
-    }
+    constructor() {}
 
     get a() {
         return this.mathForm.value.a;
@@ -35,20 +35,24 @@ export class EquationComponent implements OnInit {
         let numberSolved = 0;
         let delayMillis = 100;
 
-        this.mathForm.statusChanges.pipe(
-            filter(value => value === 'VALID'),
-            delay(delayMillis)
-            // THIS DOESN'T WORK scan((acc) => {
-            //     return {
-            //         numberSolved: acc.numberSolved + 1,
-            //         startTime: acc.startTime
-            //     }
-            //}
-            //)
-        )
+        this.mathForm.statusChanges
+            .pipe(
+                filter((value) => value === 'VALID'),
+                delay(delayMillis)
+                // THIS DOESN'T WORK scan((acc) => {
+                //     return {
+                //         numberSolved: acc.numberSolved + 1,
+                //         startTime: acc.startTime
+                //     }
+                //}
+                //)
+            )
             .subscribe(() => {
                 numberSolved++;
-                this.secondsPerSolution = (new Date().getTime() - startTime.getTime() - delayMillis) / numberSolved / 1000;
+                this.secondsPerSolution =
+                    (new Date().getTime() - startTime.getTime() - delayMillis) /
+                    numberSolved /
+                    1000;
 
                 // If we get a "valid" response, get new values for a + b and clear answer
                 // this.mathForm.controls.a.setValue(this.randomNumber());
@@ -56,15 +60,15 @@ export class EquationComponent implements OnInit {
                 // this.mathForm.controls.answer.setValue("");
 
                 // equivalent to above -- can only use this when we want to update _everything_ in form (otherwise use 'patchValue')
-                this.mathForm.setValue({a: this.randomNumber(), b: this.randomNumber(), answer: ''})
-
-
-            })
+                this.mathForm.setValue({
+                    a: this.randomNumber(),
+                    b: this.randomNumber(),
+                    answer: '',
+                });
+            });
     }
 
     randomNumber(): number {
         return Math.floor(Math.random() * 10);
     }
-
-
 }
